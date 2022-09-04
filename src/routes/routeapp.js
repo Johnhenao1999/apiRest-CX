d39a6fb386b351ac2896549cf1250f9c9f9b0222
Johnhenao1999 <jhonlm1010@hotmail.com>
2022-09-04 10:41:03 -0500

    User product list

--- src/routes/routeapp.js
+++ src/routes/routeapp.js
@@ -3,3 +3,16 @@ const router = Router();
 const products = require('../products.json');
 module.exports = router;
 
+
+/* Metodo que nos permite listar los productos
+con su precio final y los items correspondientes */
+
+router.get('/producto', (req, res) => {
+    const newItems = [];
+    for (const item of products) {
+        const precioFinal =  parseInt(item.precio) - (parseInt(item.precio) * parseFloat(item.descuento)) + (parseInt(item.precio) * parseFloat(item.iva));
+        newItems.push({ 'sku': item.sku, 'nombre': item.nombre, 'URL': item.url, 'marca': item.marca, 'Precio final': precioFinal });
+    }
+    res.json((newItems));
+
+})
