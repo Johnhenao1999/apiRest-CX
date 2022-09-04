d7fd662c47170d33785f8c820d7290aa8c9e3dc2
Johnhenao1999 <jhonlm1010@hotmail.com>
2022-09-03 23:21:50 -0500

    Update and save product

--- src/routes/routeapp.js
+++ src/routes/routeapp.js
@@ -3,31 +3,35 @@ const router = Router();
 const products = require('../products.json');
 module.exports = router;
 const fs = require('fs');
+const _ = require('underscore');
 
 // Creacion del metodo para actualizar un producto
 
-
 router.put('/admin/producto/:sku', (req, res) => {
     const { sku } = req.params;
-    const {  nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion } = req.body;
-    if (sku && nombre && precio && url && marca && descripcion && iva && descuento && inventario && fecha_creacion) {
-        array.forEach(products, (producto, i) => {
-            if (producto.id == id) {
-                producto.sku = sku;
-                producto.nombre = nombre;
-                producto.precio = precio;
-                producto.url = url;
-                producto.marca = marca;
-                producto.descripcion = descripcion;
-                producto.iva = iva;
-                producto.descuento = descuento;
-                producto.inventario = inventario;
-                producto.fecha_creacion = fecha_creacion;
+    const { nombre, precio, url, marca, descripcion, iva, descuento, inventario, fecha_creacion } = req.body;
+    // Validamos que ningun capo quede faltando
+    if (nombre && precio && url && marca && descripcion && iva && descuento && descripcion && inventario && fecha_creacion) {
+        // Nos permite recorrer el arreglo productos e ir comparando uno a uno
+        _.each(products, (product, i) => {
+            if (product.sku == sku) {
+                product.nombre = nombre;
+                product.precio = precio;
+                product.url = marca;
+                product.descripcion = descripcion;
+                product.iva = iva;
+                product.descuento = descuento;
+                product.inventario = inventario;
+                product.fecha_creacion = fecha_creacion;
+
+                // Nos permite actualizar el archivo 
+                const json_products = JSON.stringify(products);
+                fs.writeFileSync('src/products.json', json_products, 'utf-8');
+
             }
-
-        });res.json(products);
+        }); res.send('El producto se actualizo satisfactoriamente')
+        res.json(products)
     }else{
-        res.status (500).json({error: 'Malo'});
-
+        res.status(500).json({error: "Verifique los campos nuevamente"})
     }
-});
+}) 
