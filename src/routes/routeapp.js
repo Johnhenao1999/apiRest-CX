4220e8fff5e652cef3ae37263f7b00ad8decb5cc
Johnhenao1999 <jhonlm1010@hotmail.com>
2022-09-03 23:35:48 -0500

    Delete Product

--- src/routes/routeapp.js
+++ src/routes/routeapp.js
@@ -2,4 +2,22 @@ const { Router } = require('express');
 const router = Router();
 const products = require('../products.json');
 module.exports = router;
+const fs = require('fs');
+const _ = require('underscore');
 
+
+// Metodo para eliminar un producto, vista administ
+router.delete('/admin/producto/:sku', (req, res) => {
+    const { sku } = req.params;
+    // Nos permite recorrer el arreglo
+    _.each(products, (product, i) =>  {
+        if(product.sku == sku){
+            products.splice(i, 1);
+            res.send('El producto ha sido eliminado con exito');
+            // Nos permite eliminar el producto de nuestro archivo JSON
+            const json_products = JSON.stringify(products);
+            fs.writeFileSync('src/products.json', json_products, 'utf-8');
+        }res.json(products);
+
+    });
+});
