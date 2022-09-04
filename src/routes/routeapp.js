6a6556142bd4a58b07684c61fd03ab6cd5058668
Johnhenao1999 <jhonlm1010@hotmail.com>
2022-09-03 23:59:12 -0500

    Consult product

--- src/routes/routeapp.js
+++ src/routes/routeapp.js
@@ -2,4 +2,15 @@ const { Router } = require('express');
 const router = Router();
 const products = require('../products.json');
 module.exports = router;
+const _ = require('underscore');
 
+
+// Metodo para consultar un producto segun el SKU, vista administrador
+router.get('/admin/producto/:sku',(req, res) => {
+    const {sku} = req.params;
+    //Recorremos nuestro arreglo e imprimimos el producto que coincida con el SKU
+    _.each(products, (product, i) =>{
+        if (product.sku == sku)
+        res.json(product);
+    })
+})
